e2171dec2e89a23425a98cb0918477bf906b0fa3
Jay Wang <jay@zijie.wang>
2024-04-11 12:17:35 -0400

    v0.9.4
    Signed-off-by: Jay Wang <jay@zijie.wang>

--- clip2imgur.rb
+++ clip2imgur.rb
@@ -3,10 +3,25 @@ class Clip2imgur < Formula
 
   desc "Shiny new formula"
   homepage "https://github.com/xiaohk/clip2imgur"
-  url "https://files.pythonhosted.org/packages/e8/ae/cfba36a2b029549715be9247dc6c93eb4164e6ed7a9fa9b0e2822485209a/clip2imgur-0.9.3.tar.gz"
-  sha256 "873767e081731f3ee7c688dd1cd3630a47e7b00ffbff9ecc43d37bbec8ab6f44"
+  url "https://files.pythonhosted.org/packages/37/ee/fc6c8603e3e79efb2a30bfa4ae8b51ece0a86be47a5597272ef0aeb36828/clip2imgur-0.9.4.tar.gz"
+  sha256 "984888f5aa406ef4d749cad201af352846e784d681162a346e468ab380f7323c"
 
   depends_on "python3"
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/71/da/e94e26401b62acd6d91df2b52954aceb7f561743aa5ccc32152886c76c96/certifi-2024.2.2.tar.gz"
+    sha256 "0569859f95fc761b18b45ef421b1290a0f65f147e92a1e5eb3e635f9a5e4e66f"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/63/09/c1bc53dab74b1816a00d8d030de5bf98f724c52c1635e07681d312f20be8/charset-normalizer-3.3.2.tar.gz"
+    sha256 "f30c3cb33b24454a82faecaf01b19c18562b1e89558fb6c56de4d9118a032fd5"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/bf/3f/ea4b9117521a1e9c50344b909be7886dd00a519552724809bb1f486986c2/idna-3.6.tar.gz"
+    sha256 "9ecdbbd083b06798ae1e86adcbfe8ab1479cf864e4ee30fe4e46a003d12491ca"
+  end
 
   resource "markdown-it-py" do
     url "https://files.pythonhosted.org/packages/38/71/3b932df36c1a044d397a1f92d1cf91ee0a503d91e470cbd670aa66b07ed0/markdown-it-py-3.0.0.tar.gz"
@@ -38,9 +53,19 @@ class Clip2imgur < Formula
     sha256 "105254a8b04934f0bc84e9c24eb360a591aaf6535c9def5f29d92af107a9bf57"
   end
 
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
+    sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
+  end
+
   resource "rich" do
     url "https://files.pythonhosted.org/packages/b3/01/c954e134dc440ab5f96952fe52b4fdc64225530320a910473c1fe270d9aa/rich-13.7.1.tar.gz"
     sha256 "9be308cb1fe2f1f57d67ce99e95af38a1e2bc71ad9813b0e247cf7ffbcc3a432"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/7a/50/7fd50a27caa0652cd4caf224aa87741ea41d3265ad13f010886167cfcc79/urllib3-2.2.1.tar.gz"
+    sha256 "d0570876c61ab9e520d776c38acbbb5b05a776d3f9ff98a5c8fd5162a444cf19"
   end
 
   def install
